0901ff201daed0c436cc5a9faaf39cd80d29d556
brianyeh1 <nyeh@chicagobooth.edu>
2025-04-14 04:27:40 +0000

    commit

--- app.rb
+++ app.rb
@@ -2,8 +2,47 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "We tied"
+  elsif @comp_move == "scissors"
+    @outcome =  "We won"
+  elsif @comp_move == "paper"
+    @outcome = "We lost"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "We won"
+  elsif @comp_move == "scissors"
+    @outcome =  "We lost"
+  elsif @comp_move == "paper"
+    @outcome = "We tied"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome = "We lost"
+  elsif @comp_move == "scissors"
+    @outcome =  "We tied"
+  elsif @comp_move == "paper"
+    @outcome = "We won"
+  end
+  erb(:scissors)
 end
